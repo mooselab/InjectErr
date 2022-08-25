be021425145d721f49e264c85d43150b8c71dced
Gunwant Jain <mail@wantguns.dev>
2022-08-25 04:25:00 -0400

    postrun: make a failover log collector
    If the main injecterr process hangs, we can still use the postrun.py
    script as an entrypoint to sync the logs locally.
    
    Signed-off-by: Gunwant Jain <mail@wantguns.dev>

--- src/injecterr/post.py
+++ src/injecterr/post.py
@@ -30,3 +30,33 @@ def postrun(injection: Injection):
                 os.makedirs(node_logpath, exist_ok=True)
                 c.run(f"tar czf {remote_tarball} logs")
                 c.get(f"{remote_tarball}", local=f"{node_logpath}/{local_tarball}")
+
+
+    
+def standalone():
+
+    # Imports
+    from injecterr.how import How
+    from injecterr.main import parse_injections
+
+    # Stub info
+    how = How("node1", "100", "cat ~/.bashrc")
+    injections = parse_injections("injections.yml")
+    injection: Injection = Injection(
+        name="fill_disk",
+        desc="Fill Disk completely on all nodes",
+        workload="micro/wordcount",
+        dataset_size="huge",
+        how=[how]
+    )
+
+    found = False
+    for i in injections:
+        if lambda i: i.name == "fill_disk":
+            injection = i
+            found = True
+
+    if not found:
+        print("Could not find the injection in the list, using stub values")
+
+    postrun(injection)
